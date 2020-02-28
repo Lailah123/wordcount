1389f61d4581dc140593958b8877d8195d554a2c
lailah123 <1175268050@qq.com>
2020-02-28 19:02:31 +0800

    新增 整数反转

---
+++ src/test/java/Integer_Inversion/Solution.java
@@ -0,0 +1,23 @@
+package Integer_Inversion;
+
+/**
+ * leedcode#7 整数反转
+ */
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            if ((ans * 10) / 10 != ans) {
+                ans = 0;
+                break;
+            }
+            ans = ans * 10 + x % 10;
+            x = x / 10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        new Solution().reverse(1534236469);
+    }
+}
