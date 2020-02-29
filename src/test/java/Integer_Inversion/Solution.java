37ee2fd404bcd0f8fd317d39a1f4319bae09cb6c
lailah123 <1175268050@qq.com>
2020-02-29 15:52:14 +0800

    新增 图算法

--- src/test/java/Integer_Inversion/Solution.java
+++ src/test/java/Integer_Inversion/Solution.java
@@ -7,6 +7,8 @@ class Solution {
     public int reverse(int x) {
         int ans = 0;
         while (x != 0) {
+            // 做溢出判断，其中的 ans*10 ，java虚拟机内部实际上是进行了数值类型提升，
+            // 即溢出时，用long类型数据暂时存储，最后通过变窄转换，保留低32位数值得到 (ans * 10) / 10 != ans
             if ((ans * 10) / 10 != ans) {
                 ans = 0;
                 break;
@@ -18,6 +20,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        new Solution().reverse(1534236469);
+        System.out.println(new Solution().reverse(1534236469));
     }
 }
